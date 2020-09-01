946571af02153771187e5bd85328275c6c39055f
Sydney Krantz <syd107@gmail.com>
2020-09-01 10:07:32 -0400

    first commit. cleaned up boilerplate

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+// ReactDOM.render(<App />, document.querySelector('#root'));
+
+
+export default App;
